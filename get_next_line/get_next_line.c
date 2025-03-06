bcac2fa215c751141cc69bbff1eca0fdcb5a7362
Abdallah Alawad <aalawad@c1r6s8.42amman.com>
2025-03-06 08:44:03 +0300

    utils functions added

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,6 +1,60 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+# define BUFFER_SIZE 42
+
+int	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+int	ft_strchr(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*res;
+	int		len1;
+	int		len2;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	res = malloc(len1 + len2 + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		res[i++] = s2[j++];
+	res[i] = '\0';
+	return (res);
+}
+
 char	*set_next(char *buffer)
 {
 	char	*line;
@@ -118,7 +172,7 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE < = 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = read_file(fd, buffer);
 	if (!buffer)
@@ -130,10 +184,10 @@ char	*get_next_line(int fd)
 	line = set_line(buffer);
 	buffer = set_next(buffer);
 	if (!line)
-	(
+	{
 		free(buffer);
 		buffer = NULL;
 		return (NULL);
-	)
+	}
 	return (line);
 }
